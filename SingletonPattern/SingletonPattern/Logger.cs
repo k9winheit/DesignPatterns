7f635841428e3e0ddcb11cd4f0d13a47d3f28a29
Kanishka Hewapathirana <kanishka.hewapathirana@gmail.com>
2021-02-06 03:16:06 +0530

    Implement on SingletonPattern demo

---
+++ SingletonPattern/SingletonPattern/Logger.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SingletonPattern
+{
+    public class Logger
+    {
+        private static Logger logger;
+        private Logger()
+        {
+
+        }
+
+        //Provide one single object - Method 1
+        //public static Logger GetInstance()
+        //{
+        //   return logger = (logger == null) ? new Logger() : logger;
+        //}
+
+        //Provide one single object - Method 2 getter
+        public static Logger Instance
+        {
+            get{
+               return logger = (logger == null) ? new Logger() : logger;
+            }
+        }
+    }
+}
--- SingletonPattern/SingletonPattern/Program.cs
+++ SingletonPattern/SingletonPattern/Program.cs
@@ -6,7 +6,21 @@ namespace SingletonPattern
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // ## Can not create object due constructor is priavte
+            //Logger logObj1 = new Logger();        
+
+            // ## Get single object without creating main objects
+            //Logger logObj1 = Logger.GetInstance();
+            //Logger logObj2 = Logger.GetInstance();
+
+            // ## Get single object using getters
+            Logger logObj1 = Logger.Instance;
+            Logger logObj2 = Logger.Instance;
+
+            // logObj1 && logObj2 both object are same. Please check hashcode
+            Console.WriteLine("Logger Object 1 " + logObj1.GetHashCode());
+            Console.WriteLine("Logger Object 2 " + logObj2.GetHashCode());
+            Console.ReadLine();
         }
     }
 }
